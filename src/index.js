084f751689d53dc3440f3e9a0c515079ca26b1a2
syncc-codm01 <syncc.codm05@gmail.com>
2026-06-01 02:47:53 +0100

    Remove GuildPresences intent, track members on join

--- src/index.js
+++ src/index.js
@@ -30,7 +30,6 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildPresences,
   ],
 });
 

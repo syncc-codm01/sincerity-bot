a039542fd38d943a98a202afd10766e785c0e663
syncc-codm01 <syncc.codm05@gmail.com>
2026-06-01 03:39:03 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -126,7 +126,7 @@ client.on(Events.GuildMemberAdd, async (member) => {
 
   try {
     const channel = await client.channels.fetch(COUNTDOWN_CHANNEL_ID);
-    const msg = await channel.send({ embeds: [buildCountdownEmbed(member, joinedAt)] });
+    const msg = await channel.send({ content: `<@${member.id}>`, embeds: [buildCountdownEmbed(member, joinedAt)] });
     data[member.id].messageId = msg.id;
     saveData(data);
     console.log(`[Countdown] Posted countdown for ${member.user.tag}`);
